3c7a822e11ae94982e672826d2a5864557dd791a
Bill <networker747@gmail.com>
2017-06-29 02:06:55 +0000

    added changing names, dispaly incoming msgs

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const uuidV4 = require('uuid/v4');
+
+
 
 // Set the port to 3001
 const PORT = 3001;
@@ -15,16 +19,34 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
+
+
   ws.send('something');
 
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    var incomingMsg = message;
+    incomingMsg.id = uuidV4();
+    var outGoingMsg = incomingMsg;
+    wss.broadcast(outGoingMsg);
+    console.log('received: %s', JSON.parse(message).content);
+
   });
 
    ws.on('close', () => console.log('Client disconnected'));
--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import Navbar from './NavBar.jsx';
 
 const users = {
   currentUser: {name: "Bob"},
-  messages: [
-    {
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-      id:"01"
-    },
-    {
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
-      id:"02"
-    }
-  ]
+  // messages: [
+  //   {
+  //     username: "Bob",
+  //     content: "Has anyone seen my marbles?",
+  //     id:"01"
+  //   },
+  //   {
+  //     username: "Anonymous",
+  //     content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+  //     id:"02"
+  //   }
+  // ]
+  messages:[]
 };
 
 
@@ -27,21 +28,28 @@ class App extends Component {
     super(props);
     this.state = users;
   }
-
+broadcastMessage(message) {
+    this.socket.send(message);
+  }
   componentDidMount() {
     this.socket = new WebSocket('ws://localhost:3001/');
-    this.socket.addEventListener('open', (event) => {
-      console.log('IT\'S ALIVE!');
+    this.socket.addEventListener('message', (event) => {
+      const newMessage = JSON.parse(event.data);
+      const newMessages = this.state.messages.concat(newMessage);
+      this.setState({ messages: newMessages
+      });
     });
-  };
+  }
 
   makeNewMessage = (message) => {
       console.log(message.message, message.username);
-      const newMessage = {id: Date.now(),
+      const newMessage = {
         username: message.username,
-        content: message.message};
-      const messages = this.state.messages.concat(newMessage)
-      this.setState({messages: messages})
+        content: message.message
+      };
+      this.state.messages.concat(newMessage);
+      //this.setState({messages: messages})
+      this.broadcastMessage(JSON.stringify(newMessage))
   }
 
   render() {
@@ -49,7 +57,7 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        <MessageList messageList={this.state.messages}/>
+        <MessageList messageList={this.state.messages} />
         <Chatbar
           currentUser={this.state.currentUser.name}
           makeNewMessage={this.makeNewMessage}/>
